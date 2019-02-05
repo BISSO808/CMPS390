bacef422090e11a007c9850a192e10cadbeceaba
Bisso808 <bishwa.bista@selu.edu>
2019-02-05 09:36:15 -0600

    final change

--- src/sinegraph.java
+++ src/sinegraph.java
@@ -1,46 +1,110 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class sinegraph {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(forLoop());
-		whileLoop();
-		average();
+	private static Scanner input;
+	public static void main(String args[]) throws FileNotFoundException{
 		to360();
+		toAbberation();
 	}
-	public static int forLoop(){
-		int sum=0;
-		for(int i=1;i<=10 ;i++ ) {
-			sum+= i;
-		}
-		return sum;
-	}
-	public static void whileLoop(){
-		int sum2=0;
-		int num=0;
-		while(num<=10) {
-			sum2+= num;
-			num++;
-		}
-		System.out.println("The sum of 10 numbers is (using while loop)" +sum2);
-	}
-	public static void average() {
-		System.out.println(forLoop());
-		double num=(double)55/10;
-		System.out.println(num);
-	}
-	public static void to360() {
+	public static void to360() throws FileNotFoundException {
+		ArrayList<Integer> radian = new ArrayList<Integer>();
 		System.out.println("What do you want the increment");
-		Scanner input =new Scanner(System.in);
+		input = new Scanner(System.in);
 		int increment = input.nextInt();
 		System.out.println("What do you want the times");
 		int i= input.nextInt();
 		System.out.println();
 		for(int p=0; p<i;p++ ) {
 			int sum=0;
-			while(sum<360) {
+			while(sum<=360) {
+				radian.add(sum);
 				sum += increment;
-				System.out.println(sum);
+				
 			}
+			
+		}
+		printArray(radian);
+		System.out.println();
+		toRadian(radian);
+	}
+	private static void printArray(ArrayList<Integer> radian) {
+		for(int x:radian) {
+			System.out.print(x+" ");
+		}
+		
+	}
+	public static void toRadian(ArrayList radian) throws FileNotFoundException {
+		ArrayList<Double> toFile = new ArrayList<Double>();
+		double radianValue;
+		Writer writer = null;
+		for(int i=0; i<radian.size(); i++) {
+			double a= (Integer) radian.get(i);
+			radianValue  = Math.sin( Math.toRadians(a));
+			toFile.add(radianValue);
+		}
+		writeToFile(toFile);
+	}
+	public static void writeToFile(ArrayList<Double> toFile) {
+		String strFilePath = "apple.txt";
+		try {
+			PrintWriter writer = new PrintWriter("value.txt");
+			for (double element: toFile) {
+				double radian  = Math.toRadians( element )  ;
+				writer.println(Math.sin(radian));
+//				System.out.println(Math.sin(radian));
+			}
+			writer.close();
+		} catch (IOException e){
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void toAbberation() {
+		ArrayList<Double> lines = new ArrayList<Double>();
+		double average=0;
+		try {
+			File file = new File("abberation.txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+
+			while ((line = bufferedReader.readLine()) != null) {
+				Double doubleObject = new Double(line);
+				lines.add(doubleObject);
+			}
+			double sum=0;   
+
+			int n=0;
+			for(Double app:lines) {
+				sum += app;
+				n++;
+			}
+			average= (sum/(n-1));
+//			System.out.println(average);
+			fileReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		outlinerOrNot(lines,average);
+	}
+	public static void outlinerOrNot(ArrayList<Double> lines, double average) {
+		for(int i=0;i<lines.size();i++) {
+			if(Math.abs(lines.get(i)-average)>3) {
+				System.out.println(lines.get(i));
+			}
+
 		}
 	}
 }
+
+
